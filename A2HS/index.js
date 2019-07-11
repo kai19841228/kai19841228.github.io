34915338d8f8230a4b9f266845ab4d46f18334b7
kai19841228 <kai19841228@163.com>
2019-07-11 17:37:59 +0800

    调整

--- A2HS/index.js
+++ A2HS/index.js
@@ -51,53 +51,53 @@ window.addEventListener('beforeinstallprompt', (e) => {
 });
 
 // 通常你应在你的应用首次初始化的时候请求显示通知的权限
-window.addEventListener('load', function () {
-  // 首先，让我们检查我们是否有权限发出通知
-  // 如果没有，我们就请求获得权限
-  if (window.Notification && Notification.permission !== "granted") {
-    Notification.requestPermission(function (status) {
-      if (Notification.permission !== status) {
-        Notification.permission = status;
-      }
-    });
-  }
+// window.addEventListener('load', function () {
+//   // 首先，让我们检查我们是否有权限发出通知
+//   // 如果没有，我们就请求获得权限
+//   if (window.Notification && Notification.permission !== "granted") {
+//     Notification.requestPermission(function (status) {
+//       if (Notification.permission !== status) {
+//         Notification.permission = status;
+//       }
+//     });
+//   }
 
-  var button = document.getElementsByTagName('button')[0];
+//   var button = document.getElementsByTagName('button')[0];
 
-  button.addEventListener('click', function () {
-    // 如果用户同意就创建一个通知
-    if (window.Notification && Notification.permission === "granted") {
-      var n = new Notification("Hi!");
-    }
+//   button.addEventListener('click', function () {
+//     // 如果用户同意就创建一个通知
+//     if (window.Notification && Notification.permission === "granted") {
+//       var n = new Notification("Hi!");
+//     }
 
-    // 如果用户没有选择是否显示通知
-    // 注：因为在 Chrome 中我们无法确定 permission 属性是否有值，因此
-    // 检查该属性的值是否是 "default" 是不安全的。
-    else if (window.Notification && Notification.permission !== "denied") {
-      Notification.requestPermission(function (status) {
-        if (Notification.permission !== status) {
-          Notification.permission = status;
-        }
+//     // 如果用户没有选择是否显示通知
+//     // 注：因为在 Chrome 中我们无法确定 permission 属性是否有值，因此
+//     // 检查该属性的值是否是 "default" 是不安全的。
+//     else if (window.Notification && Notification.permission !== "denied") {
+//       Notification.requestPermission(function (status) {
+//         if (Notification.permission !== status) {
+//           Notification.permission = status;
+//         }
 
-        // 如果用户同意了
-        if (status === "granted") {
-          var n = new Notification("Hi!");
-        }
+//         // 如果用户同意了
+//         if (status === "granted") {
+//           var n = new Notification("Hi!");
+//         }
 
-        // 否则，我们可以让步的使用常规模态的 alert
-        else {
-          alert("Hi!");
-        }
-      });
-    }
+//         // 否则，我们可以让步的使用常规模态的 alert
+//         else {
+//           alert("Hi!");
+//         }
+//       });
+//     }
 
-    // 如果用户拒绝接受通知
-    else {
-      // 我们可以让步的使用常规模态的 alert
-      alert("Hi!");
-    }
-  });
-});
+//     // 如果用户拒绝接受通知
+//     else {
+//       // 我们可以让步的使用常规模态的 alert
+//       alert("Hi!");
+//     }
+//   });
+// });
 
 if (!window.indexedDB) {
   window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.")
