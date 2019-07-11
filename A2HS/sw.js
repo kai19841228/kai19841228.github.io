ea80e261c78178763a06a3099f290b6d8ca51034
kai19841228 <kai19841228@163.com>
2019-07-11 10:08:20 +0800

    Update sw.js

---
+++ A2HS/sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('video-store').then(function(cache) {
+      return cache.addAll([
+        '/A2HS/',
+        '/A2HS/index.html',
+        '/A2HS/index.js',
+        '/A2HS/style.css',
+        '/A2HS/images/fox1.jpg',
+        '/A2HS/images/fox2.jpg',
+        '/A2HS/images/fox3.jpg',
+        '/A2HS/images/fox4.jpg'
+      ]);
+    })
+  );
+ });
+ 
+ self.addEventListener('fetch', function(e) {
+   console.log(e.request.url);
+   e.respondWith(
+     caches.match(e.request).then(function(response) {
+       return response || fetch(e.request);
+     })
+   );
+ });
