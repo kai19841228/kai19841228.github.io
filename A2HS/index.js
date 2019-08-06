73cc86309d29f2f4a6e611e0d8f2d478b389bcaa
kai19841228 <kai19841228@163.com>
2019-08-06 18:37:57 +0800

    tz

--- A2HS/index.js
+++ A2HS/index.js
@@ -31,7 +31,7 @@ if(window.Notification) {
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/A2HS/sw.js?v=1111', {scope: './'})
+           .register('/A2HS/sw.js?v=11111', {scope: './'})
            .then(function(reg) {
              console.log('Service Worker 注册成功！作用域为: ', reg.scope); 
           });
