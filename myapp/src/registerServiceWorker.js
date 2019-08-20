d0bf453685700ca568db63f2cbc8d16277776574
kai19841228 <kai19841228@163.com>
2019-08-20 18:26:23 +0800

    更新

--- myapp/src/registerServiceWorker.js
+++ myapp/src/registerServiceWorker.js
@@ -2,21 +2,6 @@
 
 import { register } from "register-service-worker";
 
-function removeOldCache(v) {
-  var ver = 'maika-prechace-' + v
-  return caches
-      .keys()
-      .then(keys =>
-          Promise.all( // 等待所有旧的资源都清理完成
-              keys
-              .filter(key => !key.startsWith(ver)) // 过滤不需要删除的资源
-              .map(key => caches.delete(key)) // 删除旧版本资源，返回为 Promise 对象
-          )
-      )
-      .then(() => {
-          console.log('removeOldCache completed.');
-      });
-}
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
@@ -36,7 +21,6 @@ if (process.env.NODE_ENV === "production") {
     },
     updated() {
       console.log("New content is available; please refresh.");
-      removeOldCache('v1.2.1')
     },
     offline() {
       console.log(
