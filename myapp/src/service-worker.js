7252ac3e78573935ae5949d1af7543a2251f6098
kai19841228 <kai19841228@163.com>
2019-09-04 11:22:52 +0800

    更新

--- myapp/src/service-worker.js
+++ myapp/src/service-worker.js
@@ -59,41 +59,7 @@ self.addEventListener('activate', function (event) {
   ]))
 })
 self.addEventListener('fetch', function (e) {
-  // 需要缓存的xhr请求
-  var cacheRequestUrls = [
-      '/weatherApi'
-  ];
   console.log('现在正在请求：' + e.request.url);
-
-  // 判断当前请求是否需要缓存
-  var needCache = cacheRequestUrls.some(function (url) {
-      return e.request.url.indexOf(url) > -1;
-  });
-
-  /**** 这里是对XHR数据缓存的相关操作 ****/
-  if (needCache) {
-      // 需要缓存
-      // 使用fetch请求数据，并将请求结果clone一份缓存到cache
-      // 此部分缓存后在browser中使用全局变量caches获取
-      caches.open(cacheKey('api')).then(function (cache) {
-          return fetch(e.request).then(function (response) {
-              cache.put(e.request.url, response.clone());
-              return response;
-          });
-      });
-  console.log("需要缓存")
-  } else {
-    // 非api请求，直接查询cache
-    // 如果有cache则直接返回，否则通过fetch请求
-    e.respondWith(
-        caches.match(e.request).then(function (cache) {
-            return cache || fetch(e.request);
-        }).catch(function (err) {
-            console.log(err);
-            return fetch(e.request);
-        })
-    );
-  }
 })
 self.addEventListener('sync', function (e) {
 })
